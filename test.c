15693de7ca7f61a732e546972341040dc4cf5973
Riot <yy@localhost.localdomain>
2015-03-16 07:27:47 -0700

    1st Version

---
+++ test.c
@@ -0,0 +1,16 @@
+/*************************************************************************
+	> File Name: test.c
+	> Author: YY
+	> Mail: YY@gmail.com 
+	> Created Time: Mon 16 Mar 2015 06:58:12 AM PDT
+ ************************************************************************/
+
+#include<stdio.h>
+#include<stdlib,h>
+
+int main(int argc, char* argv[])
+{
+	printf("Hello world!\n");
+
+	return 0;
+}
